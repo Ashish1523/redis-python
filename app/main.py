e42a5a646d6adc864a14b2306f2811c4812d55e3
Ashish Chandra <ashish.chandra022@gmail.com>
2025-07-09 18:08:02 +0530

    Connection

--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True) # wait for client
+    connection,_=server_socket.accept()
+    connection.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
