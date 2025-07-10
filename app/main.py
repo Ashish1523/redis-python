34219f983ac3c22dfab2e837cbd3fda774c7272f
Ashish Chandra <ashish.chandra022@gmail.com>
2025-07-10 17:48:47 +0530

    comcurrency

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 import socket  # noqa: F401
+import threading  # noqa: F401
 
+BUF_SIZE = 4096  # Buffer size for receiving data
+def handle_command(client: socket.socket):
+    while chunk:=client.recv(BUF_SIZE):
+        if chunk==b"":
+            break
+        client.sendall(b"+PONG\r\n")
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,14 +15,11 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True) # wait for client
-    client_socket,_=server_socket.accept()
+    # client_socket,_=server_socket.accept()
 
     while True:
-        request: bytes = client_socket.recv(1024)
-        data: bytes = request.decode()
-
-        if "ping" in data.lower():
-            client_socket.send("+PONG\r\n".encode())
+        client_socket,client_arr= server_socket.accept()
+        threading.Thread(target=handle_command, args=(client_socket,)).start()
     
 
 if __name__ == "__main__":
